4655161543a12641d73e7d67ce4d6d922653fa23
02-017 <1287560134@qq.com>
2025-10-24 23:54:30 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202510125208
+//1287560134@qq.com
+//周钰芹
 #include <stdio.h>
-int main()
+int main ()
 {
-    printf("Hello World");
-    return 0;
+    //温度转换
+    printf ("请输入摄氏温度\n");
+    float a;
+    scanf("%f",&a);
+    printf ("根据您输入的摄氏温度，对应的华氏温度为%.1f\n",a*9/5+32);
+    //温度转换
 }
